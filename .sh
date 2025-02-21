e73b429468d3b88632a68fc81a5e2b1f0a6815c0
ErcinDedeoglu <e.dedeoglu@gmail.com>
2025-02-21 14:21:10 +0400

    refactor(.sh): update docker image name from archiver to dublok/packmate for consistency fix(.sh): remove redundant --path and --output flags in docker run commands chore(Dockerfile): remove unnecessary blank lines and root user specification for cleaner Dockerfile

--- .sh
+++ .sh
@@ -1,23 +1,26 @@
 repomix --no-file-summary --no-security-check --include "src/Dockerfile,src/archiver/archiver.go" --output "repopack.yml"
 repomix --no-file-summary --no-security-check --include "README.md,src/Dockerfile,src/archiver/archiver.go" --output "repopack.yml"
 
-docker build -t archiver:latest -f src/Dockerfile src
+docker build -t dublok/packmate:latest -f src/Dockerfile src
+
+docker run --rm \
+  -v /home/ercin/go:/source:ro \
+  -v /home/ercin/github/dublok/Docker-Volume-Backup/output:/output \
+  dublok/packmate:latest \
+  --name "backup-$(date +%Y%m%d)"
+
 
 docker run --rm \
   -v /var/lib/docker/volumes/docker-volume-test_test1_data/_data:/source:ro \
-  -v /home/ercin/test-output:/output \
+  -v /home/ercin/output:/output \
   dublok/packmate:latest \
-  --path /source \
-  --output /output \
   --name "backup-$(date +%Y%m%d)" \
   --format json
 
   docker run --rm \
   -v /var/lib/docker/volumes/docker-volume-test_test1_data/_data:/source:ro \
   -v /home/ercin/test-output:/output \
-  archiver:latest \
-  --path /source \
-  --output /output \
+  dublok/packmate:latest \
   --name "my-backup" \
   --format text
 
@@ -26,8 +29,6 @@ docker run --rm \
   -v /var/lib/docker/volumes/docker-volume-test_test1_data/_data:/source:ro \
   -v /home/ercin/test-output:/output \
   archiver:latest \
-  --path /source \
-  --output /output \
   --name "backup-no-compression" \
   --compression -2 \
   --format json
@@ -36,9 +37,7 @@ docker run --rm \
 docker run --rm \
   -v /var/lib/docker/volumes/docker-volume-test_test1_data/_data:/source:ro \
   -v /home/ercin/test-output:/output \
-  archiver:latest \
-  --path /source \
-  --output /output \
+    dublok/packmate:latest \
   --name "backup-fast" \
   --compression 1 \
   --format json
@@ -47,9 +46,7 @@ docker run --rm \
 docker run --rm \
   -v /var/lib/docker/volumes/docker-volume-test_test1_data/_data:/source:ro \
   -v /home/ercin/test-output:/output \
-  archiver:latest \
-  --path /source \
-  --output /output \
+  dublok/packmate:latest \
   --name "backup-max-compression" \
   --compression 9 \
   --format json
